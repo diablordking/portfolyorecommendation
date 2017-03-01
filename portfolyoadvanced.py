d5026cc7017f0e8f575ac25692d7cbd3346756f7
U350709048 <diablordking@gmail.com>
2017-03-01 15:51:14 +0300

    Logical Regression model countiue and n-gram model tried 2,2 so far I got 60% score

---
+++ portfolyoadvanced.py
@@ -0,0 +1,46 @@
+from sklearn.feature_extraction.text import CountVectorizer
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import PolynomialFeatures
+
+
+data = pd.read_csv('input/Combined_News_DJIA.csv')
+print(data)
+
+train = data[data['Date'] < '2015-01-01']
+test = data[data['Date'] > '2014-12-31']
+poly = PolynomialFeatures(degree=2)
+
+
+trainheadlines = []
+for row in range(0,len(train.index)):
+    trainheadlines.append(' '.join(str(x) for x in train.iloc[row,2:27]))
+
+advancedvectorizer = CountVectorizer(ngram_range=(2,2))
+#advancedtrain = poly.fit_transform(trainheadlines)
+advancedtrain = advancedvectorizer.fit_transform(trainheadlines)
+
+print(advancedtrain.shape)
+
+advancedmodel = LogisticRegression()
+advancedmodel = advancedmodel.fit(advancedtrain, train["Label"])
+
+testheadlines = []
+for row in range(0,len(test.index)):
+    testheadlines.append(' '.join(str(x) for x in test.iloc[row,2:27]))
+advancedtest = advancedvectorizer.transform(testheadlines)
+advpredictions = advancedmodel.predict(advancedtest)
+
+print(pd.crosstab(test["Label"], advpredictions, rownames=["Actual"], colnames=["Predicted"]))
+
+
+
+advwords = advancedvectorizer.get_feature_names()
+advcoeffs = advancedmodel.coef_.tolist()[0]
+advcoeffdf = pd.DataFrame({'Words' : advwords,
+                        'Coefficient' : advcoeffs})
+advcoeffdf = advcoeffdf.sort_values(['Coefficient', 'Words'], ascending=[0, 1])
+
+print(advcoeffdf.head(10))
+
+
