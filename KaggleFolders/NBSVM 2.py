bab94a2c3a8f9b094b6fa2a37b605ba39a33a7d8
U350709048 <diablordking@gmail.com>
2017-03-14 14:53:44 +0300

    NbSVM 59 accurrent with tensorflow

---
+++ KaggleFolders/NBSVM 2.py
@@ -0,0 +1,214 @@
+
+import re
+import nltk
+import pandas as pd
+import numpy as np
+
+from bs4 import BeautifulSoup
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+english_stemmer=nltk.stem.SnowballStemmer('english')
+
+from sklearn.feature_selection.univariate_selection import SelectKBest, chi2, f_classif
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer,CountVectorizer
+from sklearn.linear_model import SGDClassifier, SGDRegressor,LogisticRegression
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+import random
+import itertools
+
+import sys
+import os
+import argparse
+from sklearn.pipeline import Pipeline
+from scipy.sparse import csr_matrix
+from sklearn.feature_extraction.text import CountVectorizer
+import six
+from abc import ABCMeta
+from scipy import sparse
+from scipy.sparse import issparse
+from sklearn.base import BaseEstimator, ClassifierMixin
+from sklearn.utils import check_X_y, check_array
+from sklearn.utils.extmath import safe_sparse_dot
+from sklearn.preprocessing import normalize, binarize, LabelBinarizer
+from sklearn.svm import LinearSVC
+
+from keras.preprocessing import sequence
+from keras.utils import np_utils
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation, Lambda
+from keras.layers.embeddings import Embedding
+from keras.layers.recurrent import LSTM, SimpleRNN, GRU
+from keras.preprocessing.text import Tokenizer
+from collections import defaultdict
+from keras.layers.convolutional import Convolution1D
+from keras import backend as K
+import seaborn as sns
+import matplotlib.pyplot as plt
+from matplotlib import cm
+
+plt.style.use('ggplot')
+
+
+data = pd.read_csv('../input/Combined_News_DJIA.csv')
+train = data[data['Date'] < '2015-01-01']
+test = data[data['Date'] > '2014-12-31']
+
+
+class NBSVM(six.with_metaclass(ABCMeta, BaseEstimator, ClassifierMixin)):
+    def __init__(self, alpha=1.0, C=1.0, max_iter=10000):
+        self.alpha = alpha
+        self.max_iter = max_iter
+        self.C = C
+        self.svm_ = []  # fuggly
+
+    def fit(self, X, y):
+        X, y = check_X_y(X, y, 'csr')
+        _, n_features = X.shape
+
+        labelbin = LabelBinarizer()
+        Y = labelbin.fit_transform(y)
+        self.classes_ = labelbin.classes_
+        if Y.shape[1] == 1:
+            Y = np.concatenate((1 - Y, Y), axis=1)
+
+        # LabelBinarizer().fit_transform() returns arrays with dtype=np.int64.
+        # so we don't have to cast X to floating point
+        Y = Y.astype(np.float64)
+
+        # Count raw events from data
+        n_effective_classes = Y.shape[1]
+        self.class_count_ = np.zeros(n_effective_classes, dtype=np.float64)
+        self.ratios_ = np.full((n_effective_classes, n_features), self.alpha,
+                               dtype=np.float64)
+        self._compute_ratios(X, Y)
+
+        # flugglyness
+        for i in range(n_effective_classes):
+            X_i = X.multiply(self.ratios_[i])
+            svm = LinearSVC(C=self.C, max_iter=self.max_iter)
+            Y_i = Y[:, i]
+            svm.fit(X_i, Y_i)
+            self.svm_.append(svm)
+
+        return self
+
+    def predict(self, X):
+        n_effective_classes = self.class_count_.shape[0]
+        n_examples = X.shape[0]
+
+        D = np.zeros((n_effective_classes, n_examples))
+
+        for i in range(n_effective_classes):
+            X_i = X.multiply(self.ratios_[i])
+            D[i] = self.svm_[i].decision_function(X_i)
+
+        return self.classes_[np.argmax(D, axis=0)]
+
+    def _compute_ratios(self, X, Y):
+        """Count feature occurrences and compute ratios."""
+        if np.any((X.data if issparse(X) else X) < 0):
+            raise ValueError("Input X must be non-negative")
+
+        self.ratios_ += safe_sparse_dot(Y.T, X)  # ratio + feature_occurrance_c
+        normalize(self.ratios_, norm='l1', axis=1, copy=False)
+        row_calc = lambda r: np.log(np.divide(r, (1 - r)))
+        self.ratios_ = np.apply_along_axis(row_calc, axis=1, arr=self.ratios_)
+        check_array(self.ratios_)
+        self.ratios_ = sparse.csr_matrix(self.ratios_)
+
+        # p_c /= np.linalg.norm(p_c, ord=1)
+        # ratios[c] = np.log(p_c / (1 - p_c))
+
+
+def f1_class(pred, truth, class_val):
+    n = len(truth)
+
+    truth_class = 0
+    pred_class = 0
+    tp = 0
+
+    for ii in range(0, n):
+        if truth[ii] == class_val:
+            truth_class += 1
+            if truth[ii] == pred[ii]:
+                tp += 1
+                pred_class += 1
+                continue;
+        if pred[ii] == class_val:
+            pred_class += 1
+
+    precision = tp / float(pred_class)
+    recall = tp / float(truth_class)
+
+    return (2.0 * precision * recall) / (precision + recall)
+
+
+def semeval_senti_f1(pred, truth, pos=2, neg=0):
+    f1_pos = f1_class(pred, truth, pos)
+    f1_neg = f1_class(pred, truth, neg)
+
+    return (f1_pos + f1_neg) / 2.0;
+
+
+def main(train_file, test_file, ngram=(1, 3)):
+    print('loading...')
+    train = pd.read_csv(train_file, delimiter='\t', encoding='utf-8', header=0,
+                        names=['text', 'label'])
+
+    # to shuffle:
+    # train.iloc[np.random.permutation(len(df))]
+
+    test = pd.read_csv(test_file, delimiter='\t', encoding='utf-8', header=0,
+                       names=['text', 'label'])
+
+    print('vectorizing...')
+    vect = CountVectorizer()
+    classifier = NBSVM()
+
+    # create pipeline
+    clf = Pipeline([('vect', vect), ('nbsvm', classifier)])
+    params = {
+        'vect__token_pattern': r"\S+",
+        'vect__ngram_range': ngram,
+        'vect__binary': True
+    }
+    clf.set_params(**params)
+
+    # X_train = vect.fit_transform(train['text'])
+    # X_test = vect.transform(test['text'])
+
+    print('fitting...')
+    clf.fit(train['text'], train['label'])
+
+    print('classifying...')
+    pred = clf.predict(test['text'])
+
+    print('testing...')
+    acc = accuracy_score(test['label'], pred)
+    f1 = semeval_senti_f1(pred, test['label'])
+    print('NBSVM: acc=%f, f1=%f' % (acc, f1))
+
+
+
+trainheadlines = []
+for row in range(0,len(train.index)):
+    trainheadlines.append(' '.join(str(x) for x in train.iloc[row,2:27]))
+
+
+advancedvectorizer = TfidfVectorizer( min_df=0.031, max_df=0.2, max_features = 200000, ngram_range = (2, 2))
+advancedtrain = advancedvectorizer.fit_transform(trainheadlines)
+print(advancedtrain.shape)
+
+advancedmodel = NBSVM(C=0.01)
+advancedmodel = advancedmodel.fit(advancedtrain, train["Label"])
+testheadlines = []
+for row in range(0,len(test.index)):
+    testheadlines.append(' '.join(str(x) for x in test.iloc[row,2:27]))
+advancedtest = advancedvectorizer.transform(testheadlines)
+preds13 = advancedmodel.predict(advancedtest)
+acc13 = accuracy_score(test['Label'], preds13)
+
+print('NBSVM 2: ', acc13)
